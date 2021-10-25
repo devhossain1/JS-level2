7fda8241e8d6be5852178855fe025b9226fe9321
hossain377 <hossainahmed377@gmail.com>
2021-10-25 07:10:16 +0600

    jquery api calling

---
+++ index.js
@@ -0,0 +1,35 @@
+
+const makeRequest = async(url,method,data) =>{
+    try{
+        const result= await $.ajax({
+            url:url,
+            method:method,
+            data:data,
+        });
+        return result;
+    }catch(err){
+        console.log(err);
+    }
+};
+const deleteData = ()=>{
+    makeRequest("https://jsonplaceholder.typicode.com/posts/1",'DELETE')
+    .then((res)=>console.log(res));
+}
+deleteData();
+// const createData = ()=>{
+//     makeRequest("https://jsonplaceholder.typicode.com/posts/1",'PUT',{
+//         id:1,
+//         title: 'fooma',
+//         body: 'barma',
+//         userId: 1,
+//       })
+//     .then((res)=>console.log(res));
+
+// }
+// createData();
+// const getData = ()=>{
+//     makeRequest("https://jsonplaceholder.typicode.com/posts/1",'GET')
+//     .then((res)=>console.log(res));
+
+// }
+// getData();
