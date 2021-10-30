67ad20179f3faf9297b1980e0cde6aceba8c772c
hossain377 <hossainahmed377@gmail.com>
2021-10-30 08:08:38 +0600

    Udemy basic part: if, switch

--- index.js
+++ index.js
@@ -1,17 +1,72 @@
+// var firstName, lastName;
+// firstName= "hossain";
+// lastName = "ahmed"
 
-const data = require('./friends_data.json');
+// //alert(`${firstName} ${lastName} is the full name`);
+// var lastName= prompt('what is the last name');
+// console.log(firstName + ' ' + lastName);
 
-// console.log(data.students[0].friends[0].name);
-// console.log(data.students[1].language[1]);
-// for(x in data){
-//     //console.log(x);
-//     console.log(data[x]);
-// }
+let massmark = 78;
+let heightmark = 1.69;
+let massjohn = 92;
+let heightjohn = 1.92;
+let bmiMark = massmark/(heightmark*heightmark);
+let bmiJohn = massjohn/(heightjohn*heightjohn);
+console.log(bmiMark, bmiJohn);
+let hegherThan = bmiMark>bmiJohn;
+console.log(`is it true or false? = ${hegherThan}`);
 
-const data2 ={
-    name:"anis",
-    age:23
+//ternary operator
+var age=18;
+
+let drink = age >= 18 ? 'he is adult' : 'he is boy'
+console.log(drink);
+
+var job = "driver";
+switch(job){
+    case 'teacher':
+        console.log(`john is a good teacher`);
+        break;
+    case 'driver':
+        console.log(`john is a good driver`);
+        break; 
+    case 'labour':
+        console.log(`john is a good labour`);
+        break;
+    default:
+        console.log("He does anather job");        
 }
 
+switch(true){
+    case age < 13:
+        console.log("john is a boy"); 
+        break;
+    case age >=13 && age<20:
+        console.log("john is a adult");
+        break;
+    case age>=20 && age<30:
+        console.log("john is a young");
+        break;
+    default:
+        console.log('he is anather man');    
+}
 
-console.log(JSON.parse('{ "name":"hossain", "age":23 }'));
+var height;
+height = 22;
+if(height || height===0){
+    console.log("variable is defined");
+}else{
+    console.log("variable is not defined");
+}
+
+let johnScore = (15+20+45)/2;
+let mikeScore = (25+30+45)/2;
+let maryScore = (35+50+45)/2;
+console.log(johnScore,mikeScore,maryScore);
+if(mikeScore>johnScore && mikeScore>maryScore){
+    console.log(` mike ${mikeScore} is winner`);
+}else if(maryScore>johnScore && maryScore>mikeScore){
+    console.log(` mary ${maryScore} is winner`);
+}else{
+    console.log('no one can win');
+}
