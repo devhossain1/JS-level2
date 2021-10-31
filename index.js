3db7b96c35646991e306c12294d6551960478636
hossain377 <hossainahmed377@gmail.com>
2021-11-01 05:50:50 +0600

    tips calculator & total bills

--- index.js
+++ index.js
@@ -1,111 +1,75 @@
 
-function calculateAge(birthYear){
-    return 2021-birthYear;
-}
-let johnAge=calculateAge(1992);
-console.log(johnAge);
+//object and array problem solving
+let john = {
+    fullName:"john smith",
+    bills:[124, 48, 268, 180, 42],
+    calcTips:function(){
+        this.tips=[];
+        this.finalValues=[];
+        for(let i=0; i<this.bills.length; i++){
+            //determine the percentage based tipping rules
+            let percentage;
+            let bill= this.bills[i];
+            if(bill<50){
+                percentage=.2;
+            }else if(bill>=50 && bill<200){
+                percentage = .15;
+            }else{
+                percentage =.1;
+            }
+            //add result to the corresponding array
+            this.tips[i]=bill*percentage;
+            this.finalValues[i]=bill+ bill*percentage;
+        }
 
-function untilRetirement(year, firstName){
-    let age = calculateAge(year);
-    let retirement = 60-age;
-    if(retirement>0){
-        console.log(firstName + ' retires in ' + retirement +' years');
-    }else{
-        console.log( firstName +' is already retired');
-    }
-    
-
-}
-untilRetirement(1992, 'hossain');
-untilRetirement(1980, 'john');
-untilRetirement(1960, 'karim');
-
-//function expression
-var whatDoYouDo = function(job,firstName){
-    switch(job){
-      case 'teacher':
-          return firstName+ ' is a good teacher';
-      case 'driver':
-          return firstName + ' is a good driver';
-      case 'designer':
-          return firstName + 'is the best designer';       
-      default:
-          return firstName + 'does something else';
     }
 }
-console.log(whatDoYouDo('teac', 'john'));
-
-function tipCalculator(bill){
-    let percentage;
-    if(bill<50){
-        percentage = .20;
-    }else if(bill >= 50 && bill<200){
-        percentage=.15;
-    }else{
-        percentage = .1;
+let mark = {
+    fullName:"mark smith",
+    bills:[77, 475, 110, 45],
+    calcTips:function(){
+        this.tips=[];
+        this.finalValues=[];
+        for(let i=0; i<this.bills.length; i++){
+            //determine the percentage based tipping rules
+            let percentage;
+            let bill= this.bills[i];
+            if(bill<100){
+                percentage=.2;
+            }else if(bill>=100 && bill<300){
+                percentage = .15;
+            }else{
+                percentage =.25;
+            }
+            //add result to the corresponding array
+            this.tips[i]=bill*percentage;
+            this.finalValues[i]=bill+ bill*percentage;
+        }
     }
-    return percentage * bill;
 }
 
-var bills= [124,48,268];
-let tips =[tipCalculator(bills[0]),
-          tipCalculator(bills[1]),
-          tipCalculator(bills[2])];
-
-let finalValue =[bills[0] + tips[0],
-                bills[0] + tips[0],
-                bills[0] + tips[0]];              
-console.log(tips, finalValue);
-
-// var john = {
-//    firstName:'john',
-//    lastName:'Doe',
-//    birthYear:1992,
-//    calcAge: function(){
-//        this.age= 2021-this.birthYear;
-//    }
-// }
-// john.calcAge();
-//console.log(john);
-
-//chalange for object
-var john = {
-    fullName:'john doe',
-    mass:75,
-    height:1.70,
-    calcBMI:function(){
-        this.bmi= this.mass/(this.height*this.height);
-        return this.bmi;
+function calcAverage(tips){
+    var sum = 0;
+    for(i=0; i<tips.length; i++){
+       sum = sum + tips[i];
     }
- }
-
- var mark = {
-    fullName:'mark doe',
-    mass:95,
-    height:1.75,
-    calcBMI:function(){
-        this.bmi= this.mass/(this.height*this.height);
-        return this.bmi;
-    }
- }
-
-// john.calcBMI();
-// mark.calcBMI();
-
-
-if(john.calcBMI() > mark.calcBMI()){
-    console.log(john.fullName + " has a higher BMI of " + john.bmi);
-}else if(mark.bmi > john.bmi){
-    console.log(john.fullName + " has a higher BMI of " + mark.bmi); 
-}else{
-    console.log('they have the same BMI');
+    return sum / tips.length;
 }
-let johna = ['john', 'smith', 1234, true, 'blue'];
-// for(let i=0; i<johna.length; i++){
-//     if( typeof johna[i] !=='string') continue;
-//     console.log(johna[i]);
-// }
 
-for(let i=johna.length-1; i>=0; i--){
-    console.log(johna[i]);
+
+john.calcTips();
+mark.calcTips();
+
+john.average=calcAverage(john.tips);
+mark.average=calcAverage(mark.tips);
+console.log(john,mark);
+if(john.average>mark.average){
+    console.log(john.fullName + '\'s family pay higher tips , \
+    with an average $'+ john.average);
+
+}else if(mark.average>john.average){
+    console.log(john.fullName + '\'s family pay higher tips , with \
+    an average $'+ mark.average);
+
 }
+
